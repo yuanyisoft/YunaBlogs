6e36a0f933f935bdfb557721eef15297fb7f8623
yuanyi <iynauy@qq.com>
2017-11-02 23:58:23 +0800

    样式修改

--- YunaBlogs/App_Start/RouteConfig.cs
+++ YunaBlogs/App_Start/RouteConfig.cs
@@ -14,10 +14,24 @@ namespace YunaBlogs
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional}              
+            name: "Default",
+            url: "{controller}/{action}/{id}",
+            defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+
             );
+
+
+            //routes.MapRoute(
+            //"test",
+            //"{action}",
+            //new { controller = "Admin", action = "Login" }
+            //);
+
+            //routes.MapRoute(
+            //"Default",
+            //"{controller}/{action}",
+            //new { controller = "Home", action = "Index" }
+            //);
         }
     }
 }
